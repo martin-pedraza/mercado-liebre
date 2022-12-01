52202d343a25440fc17ae8d2af2673e844625e59
martin-pedraza <pedraza.martindiego@gmail.com>
2022-12-01 13:10:48 -0300

    format

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ const app = express();
 const pathStatic = path.resolve(__dirname, './public');
 app.use(express.static(pathStatic));
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log('Corriendo en puerto 3000')
+app.listen(process.env.PORT || 3000, () => {
+	console.log('Corriendo en puerto 3000');
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'))
+	res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'))
+	res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html'))
+	res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
